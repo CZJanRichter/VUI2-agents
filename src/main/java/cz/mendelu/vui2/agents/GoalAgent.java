90e4ea481ee6ecdfdacda5e11bcba4afb67b5a6b
Jan Richter <32869462+CZJanRichter@users.noreply.github.com>
2019-01-02 11:23:51 +0100

    START state - gets to the wall in front of the robot

--- src/main/java/cz/mendelu/vui2/agents/GoalAgent.java
+++ src/main/java/cz/mendelu/vui2/agents/GoalAgent.java
@@ -30,6 +30,7 @@ public class GoalAgent extends AbstractAgent {
     AgentState state = AgentState.START;
     AgentDirection direction = AgentDirection.NORTH;
     Room currentRoom = new Room(0,0, RoomState.DOCK);
+    Room edgeCheckHook;
 
     ArrayList<Room> memory = new ArrayList<Room>();
 
@@ -41,11 +42,18 @@ public class GoalAgent extends AbstractAgent {
     public Action doAction(boolean canMove, boolean dirty, boolean dock) {
         this.turnNo++;
         this.addUnknownRoomsToMemory();
-        this.setCurrentRoomState(dirty);
-        this.printInfo();
+        //this.printInfo();
+        if (dirty) return this.clean();
+        if (!dock) this.currentRoom.state = RoomState.CLEAN;
         switch(this.state){
             case START:
-                break;
+                if (this.canMoveForward(canMove)) {
+                    return this.forward();
+                } else {
+                    this.state = AgentState.EDGE_CHECK;
+                    this.edgeCheckHook = this.currentRoom;
+                    return this.turnRight();
+                }
             case EDGE_CHECK:
                 break;
             case EXPLORE:
@@ -71,6 +79,12 @@ public class GoalAgent extends AbstractAgent {
     }
 
     public Action forward() {
+        switch(this.direction) {
+            case EAST: this.currentRoom = this.getRoomFromMemory(this.currentRoom.x+1, this.currentRoom.y); break;
+            case SOUTH: this.currentRoom = this.getRoomFromMemory(this.currentRoom.x, this.currentRoom.y-1); break;
+            case WEST: this.currentRoom = this.getRoomFromMemory(this.currentRoom.x-1, this.currentRoom.y); break;
+            case NORTH: this.currentRoom = this.getRoomFromMemory(this.currentRoom.x, this.currentRoom.y+1); break;
+        }
         return Action.FORWARD;
     }
 
@@ -78,6 +92,12 @@ public class GoalAgent extends AbstractAgent {
         this.turnsRightStreak = 0;
         this.turnsLeftStreak++;
         this.turnsLeft++;
+        switch(this.direction) {
+            case EAST: this.direction = AgentDirection.NORTH; break;
+            case NORTH: this.direction = AgentDirection.WEST; break;
+            case WEST: this.direction = AgentDirection.SOUTH; break;
+            case SOUTH: this.direction = AgentDirection.EAST; break;
+        }
         return Action.TURN_LEFT;
     }
 
@@ -85,6 +105,12 @@ public class GoalAgent extends AbstractAgent {
         this.turnsLeftStreak = 0;
         this.turnsRightStreak++;
         this.turnsRight++;
+        switch(this.direction) {
+            case EAST: this.direction = AgentDirection.SOUTH; break;
+            case SOUTH: this.direction = AgentDirection.WEST; break;
+            case WEST: this.direction = AgentDirection.NORTH; break;
+            case NORTH: this.direction = AgentDirection.EAST; break;
+        }
         return Action.TURN_RIGHT;
     }
 
@@ -99,16 +125,18 @@ public class GoalAgent extends AbstractAgent {
             this.memory.add(new Room(this.currentRoom.x, this.currentRoom.y-1, RoomState.UNKNOWN)); // south
     }
 
+    public Room getRoomFromMemory(int x, int y) {
+        for (Room r : this.memory) {
+            if (r.x == x && r.y == y) return r;
+        }
+        return null; // shouldn't ever happen, use isRoomInMemory() to check before using get
+    }
+
     public boolean isRoomInMemory(int x, int y) {
         for (Room r : this.memory) {
             if (r.x == x && r.y == y) return true;
         }
         return false;
-    }
-
-    public void setCurrentRoomState(boolean dirty) {
-        if (this.currentRoom.state == RoomState.UNKNOWN)
-            this.currentRoom.state = dirty ? RoomState.MESSY : RoomState.CLEAN;
     }
 
     public void printInfo() {
